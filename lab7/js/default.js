1ae2e2ca8af16d30030999a7009ef1324f64740a
Volodymyr Khymych <51357944+vovik541@users.noreply.github.com>
2023-01-06 17:35:19 +0100

    reload / stop / start / close modified for default.js

--- lab7/js/default.js
+++ lab7/js/default.js
@@ -14,6 +14,7 @@ let startingHeight;
 
 function playDefault() {
     hideBlock("defaultReload");
+    hideBlock("defaultStop");
     displayBlock("defaultWork");
     displayBlock("defaultClose");
     displayBlock("defaultStart");
@@ -29,15 +30,18 @@ function playDefault() {
 
 function closeDefault() {
     hideBlock("defaultWork");
+    isStopped = true;
 }
 
+let isStopped;
 function startDefault() {
     hideBlock("defaultStart");
-    displayBlock("defaultReload");
+    displayBlock("defaultStop");
 
     let fromTop = startingHeight;
     let fromLeft = width;
     let isBottomDirection = true;
+    isStopped = false;
 
     let interval = setInterval(
         function () {
@@ -57,6 +61,9 @@ function startDefault() {
             if (fromLeft < -50) {
                 clearInterval(interval)
             }
+            if (isStopped){
+                clearInterval(interval)
+            }
 
             square.style.top = fromTop + "px";
             square.style.left = fromLeft + "px";
@@ -65,11 +72,22 @@ function startDefault() {
 }
 
 function reloadDefault() {
+    hideBlock("defaultReload");
+    displayBlock("defaultStart");
 
+    let animField = document.querySelector("#defaultAnimationField");
+    square = document.querySelector("#square-box");
+    width = animField.offsetWidth - 15;
+    height = animField.offsetHeight;
+    square.style.left = width + "px";
+    startingHeight = document.querySelector("#defaultControls").offsetHeight + 5;
+    square.style.top = "";
 }
 
 function stopDefault() {
-
+    hideBlock("defaultStop");
+    displayBlock("defaultReload");
+    isStopped = true;
 }
 
 function displayBlock(blockName) {
@@ -82,4 +100,23 @@ function hideBlock(blockName) {
     document.getElementById(blockName)
         .style
         .display = "none";
+}
+
+function printMessageToConsole(message) {
+    let output = document.getElementById("defaultEventsOutput");
+    output.textContent = message;
+
+    eventsLogs[eventsLogs.length] = {
+        message: message,
+        time: getCurrentDayTime()
+    }
+    localStorage.setItem("defaultEventsLogs", JSON.stringify(eventsLogs));
+}
+
+function getCurrentDayTime() {
+    let today = new Date();
+    let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    return date + " " + time;
 }
