ffde7537eca942aaf53d12fa17e1c7b71dc3b70b
Volodymyr Khymych <51357944+vovik541@users.noreply.github.com>
2023-01-06 02:35:40 +0100

    start prepared

--- lab7/js/default.js
+++ lab7/js/default.js
@@ -1,5 +1,27 @@
+let defaultEventsLogs = [];
+
+window.onload = (event) => {
+    defaultEventsLogs = JSON.parse(localStorage.getItem("defaultEventsLogs"));
+    if (defaultEventsLogs == null) {
+        defaultEventsLogs = [];
+    }
+}
 function playDefault(){
 
 }
 
+function closeDefault(){
 
+}
+
+function startDefault(){
+
+}
+
+function reloadDefault(){
+
+}
+
+function stopDefault(){
+
+}
