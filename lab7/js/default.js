b63c618cd9bd13cbc79f28deab03b16fd03b0049
Volodymyr Khymych <51357944+vovik541@users.noreply.github.com>
2023-01-06 16:52:18 +0100

    default movement implemented

--- lab7/js/default.js
+++ lab7/js/default.js
@@ -6,22 +6,80 @@ window.onload = (event) => {
         defaultEventsLogs = [];
     }
 }
-function playDefault(){
+
+let square;
+let width;
+let height;
+let startingHeight;
+
+function playDefault() {
+    hideBlock("defaultReload");
+    displayBlock("defaultWork");
+    displayBlock("defaultClose");
+    displayBlock("defaultStart");
+
+    let animField = document.querySelector("#defaultAnimationField");
+    square = document.querySelector("#square-box");
+    width = animField.offsetWidth - 15;
+    height = animField.offsetHeight;
+    square.style.left = width + "px";
+    startingHeight = document.querySelector("#defaultControls").offsetHeight + 5;
+    square.style.top = startingHeight;
+}
+
+function closeDefault() {
+    hideBlock("defaultWork");
+}
+
+function startDefault() {
+    hideBlock("defaultStart");
+    displayBlock("defaultReload");
+
+    let fromTop = startingHeight;
+    let fromLeft = width;
+    let isBottomDirection = true;
+
+    let interval = setInterval(
+        function () {
+            if (isBottomDirection) {
+                if (fromTop > height + startingHeight - 25) {
+                    isBottomDirection = false;
+                }
+                fromTop += 5;
+                fromLeft -= 3;
+            } else {
+                if (fromTop < startingHeight + 5) {
+                    isBottomDirection = true;
+                }
+                fromTop -= 5;
+                fromLeft -= 3;
+            }
+            if (fromLeft < -50) {
+                clearInterval(interval)
+            }
+
+            square.style.top = fromTop + "px";
+            square.style.left = fromLeft + "px";
+        }, 15);
 
 }
 
-function closeDefault(){
+function reloadDefault() {
 
 }
 
-function startDefault(){
+function stopDefault() {
 
 }
 
-function reloadDefault(){
-
+function displayBlock(blockName) {
+    document.getElementById(blockName)
+        .style
+        .display = "block";
 }
 
-function stopDefault(){
-
+function hideBlock(blockName) {
+    document.getElementById(blockName)
+        .style
+        .display = "none";
 }
